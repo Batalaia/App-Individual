56a64f1dbe8beab386d0e38fb3bc71843f3facd7
Batalaia <bernardo.atalaia@gmail.com>
2023-04-07 16:37:07 +0100

    as

--- src/main/java/appindividual/resources/Update.java
+++ src/main/java/appindividual/resources/Update.java
@@ -12,8 +12,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
